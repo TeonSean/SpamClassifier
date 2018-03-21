fba9cb34e543c746ebd7aea24126c9d0e663591d
Teon Sean <416152680@qq.com>
2018-03-21 10:10:05 +0800

    Simplified the way number be converted to str

--- WordCollector.py
+++ WordCollector.py
@@ -2,13 +2,7 @@ count = 64620
 root_dir = '.\\data\\data_cut\\'
 
 def numberToStr3(n):
-	if n < 10:
-		n = '00' + str(n)
-	elif n < 100:
-		n = '0' + str(n)
-	else:
-		n = str(n)
-	return n
+	return '%03d' % n
 
 def getFilenameByNumber(n):
 	if n >= count:
@@ -16,4 +10,3 @@ def getFilenameByNumber(n):
 	level_1 = n // 300
 	level_2 = n % 300
 	return root_dir + numberToStr3(level_1) + '\\' + numberToStr3(level_2)
-
